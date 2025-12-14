075909a055a360b5bdd1630cc4a5ec972231a5c9
Harvey13 <h.pipon@wanadoo.fr>
2025-12-14 18:14:55 +0100

    feat: Version 0.00: init

--- Pages/home.jsx
+++ Pages/home.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input.jsx";
+import { Button } from "@/components/ui/button.jsx";
 import { Download, QrCode, Sparkles } from "lucide-react";
 import QRCodeLib from "qrcode";
 
